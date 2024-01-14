f126d3cf64fe01074d19209b318badcf9f71d2d7
Varshitha Reddy <45087191+shitha17@users.noreply.github.com>
2024-01-14 13:11:53 -0500

    Time: 726 ms (12.76%), Space: 0B (100.00%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,10 @@
+WITH triangle_cte AS (
+    SELECT 
+        x, y, z,
+        CASE 
+            WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+            ELSE 'No'
+        END AS Triangle
+    FROM Triangle
+)
+SELECT * FROM triangle_cte;
